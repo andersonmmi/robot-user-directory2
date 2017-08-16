cac1effc116e02f1256f9e2fc6c25c0ee38f1d42
Aaron Anderson <aaronlynanderson@gmail.com>
2017-08-16 15:31:42 -0400

    commit app.js, index.html, styles.css and package.json

---
+++ app.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const moment = require('moment');
+const chalk = require('chalk');
+const express = require('express');
+const app = express();
+const port = 3000;
+const mustache = require('mustache-express');
+
+app.engine('mustache', mustache());
+app.set('vews', './views');
+app.set('view engine', 'mustache');
